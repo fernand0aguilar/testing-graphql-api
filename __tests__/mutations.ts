f778475640f6b9f9439d59dfab3e02d751f4b7b9
Fernando Aguilar <fernando.aguilar@hotmail.com.br>
2020-12-03 02:09:39 +0100

    Feat: add test suit to breat saveUserMutation

--- __tests__/mutations.ts
+++ __tests__/mutations.ts
@@ -189,3 +189,67 @@ describe('test user CRUD', () => {
       })
   })
 })
+
+describe('breaking create user function', () => {
+   
+  testCaseToVerifyUserNotInList()
+
+  test("It should send mutationToSaveNewUser with wrong password format and return error", async (done) => {
+    request
+    .post('/')
+    .send({
+      query: `mutation {
+      SaveUser(
+          input: {
+          username: "${testUserCredentialsInput.username}", 
+          password: "withoutnumbersnorspecialcharactersnoruppercase", 
+          role: "${testUserCredentialsInput.role}"
+        }
+      )
+      }`
+    })
+    .expect(200)
+    .end((err, res) => {
+      expect(res.body.data.SaveUser).toBeNull()
+      expect(res.body.errors[0].message).toBe("Password does not conform to policy: Password must have uppercase characters")
+      done()
+    })
+  })
+
+  testCaseToVerifyUserNotInList()
+ 
+  test("It should send mutationToSaveNewUser with right password format this time", 
+  async (done) => {
+    request
+    .post('/')
+    .send({
+      query: `mutation {
+      SaveUser(
+          input: {
+          username: "${testUserCredentialsInput.username}", 
+          password: "${testUserCredentialsInput.password}", 
+          role: "${testUserCredentialsInput.role}"
+        }
+      )
+      }`
+    })
+    .expect(200)
+    .end((err, res) => {
+      if(err) return done(err)
+      expect(res.body.data.SaveUser).toEqual(true)
+      done()
+    })
+  })
+
+  test("It should delete the user and return true", async (done) => {
+    request
+    .post('/')
+    .send(mutationToDeleteUser(testUserCredentialsInput.username))
+    .expect(200)
+    .end((err, res) => {
+      if(err) return done(err)
+      expect(res.body.data.DeleteUser).toEqual(true)
+      done()
+    })
+  })
+})
